703b4c9089df15148f057a1093297c8adcf35222
naganggaleann1158-ctrl <nagangga.leann690@gmail.com>
2026-05-27 20:58:51 +0800

    Redirect root URL to /api/ to fix 404 on base path

--- backend/inventory_backend/urls.py
+++ backend/inventory_backend/urls.py
@@ -18,8 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
